8a2529ad51c3eeaaf631ea00efdb97036b32bc1a
jungsang1675 <kok3723@naver.com>
2023-05-20 06:09:46 +0900

    CRUD 연습

--- jsw_boot/ex01/src/main/java/com/example/ex01/mapper/ProductMapper.java
+++ jsw_boot/ex01/src/main/java/com/example/ex01/mapper/ProductMapper.java
@@ -4,6 +4,9 @@ import com.example.ex01.domain.ProductVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import javax.swing.plaf.PanelUI;
+import java.util.List;
+
 @Mapper
 public interface ProductMapper {
     // 상품추가
@@ -12,7 +15,9 @@ public interface ProductMapper {
     @Select("SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_STOCK, PRODUCT_PRICE, REGISTER_DATE, UPDATE_DATE FROM TBL_PRODUCT WHERE PRODUCT_ID = #{productId}")
     public ProductVO select(long productId);
     // 상품 수정
+    public void update(ProductVO productVO);
     // 상품 삭제
+    public void delete(long productId);
     // 상품 전체 조회
-
+    public List<ProductVO> selectAll();
 }
--- jsw_boot/ex01/src/test/java/com/example/ex01/mapper/MapperTests.java
+++ jsw_boot/ex01/src/test/java/com/example/ex01/mapper/MapperTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 @Slf4j
 public class MapperTests {
@@ -21,7 +23,7 @@ public class MapperTests {
     @Test
     public void insertTest(){
         ProductVO productVO = new ProductVO();
-        productVO.setProductName("감자");
+        productVO.setProductName("감자고구마");
         productVO.setProductPrice(1500L);
         productVO.setProductStock(400L);
         productMapper.insert(productVO);
@@ -29,7 +31,29 @@ public class MapperTests {
 
     @Test
     public void selectTest(){
-        ProductVO productVO = productMapper.select(1L);
+        ProductVO productVO = productMapper.select(2L);
         log.info(productVO.toString());
+        assertThat(productVO.getProductPrice()).isEqualTo(1500L);
+    }
+
+    @Test
+    public void updateTest() {
+        ProductVO productVO = productMapper.select(1L);
+        productVO.setProductName("고구마");
+        productVO.setProductPrice(3000L);
+
+        productMapper.update(productVO);
+    }
+
+    @Test
+    public void deleteTest() {
+        productMapper.delete(1L);
+    }
+
+    @Test
+    public void selectAllTest() {
+        productMapper.selectAll().stream().map(ProductVO::toString).forEach(log::info);
+         assertThat(productMapper.selectAll().get(0).getProductName()).isEqualTo("감자");
+//        assertThat(productMapper.select(2L)).isIn(productMapper.selectAll());
     }
 }
